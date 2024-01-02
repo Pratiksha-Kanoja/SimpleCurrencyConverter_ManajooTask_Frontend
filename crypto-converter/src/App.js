d5e391e8d41bb19fbb81db153e23e01ad7b47058
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2024-01-02 23:42:38 +0530

    new

--- crypto-converter/src/App.js
+++ crypto-converter/src/App.js
@@ -1,22 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './App.css'
 
 function App() {
     const [currencies, setCurrencies] = useState([]);
     const [sourceCrypto, setSourceCrypto] = useState('');
     const [amount, setAmount] = useState('');
     const [targetCurrency, setTargetCurrency] = useState('USD');
-    const [convertedAmount, setConvertedAmount] = useState(0);
+    const [convertedAmount, setConvertedAmount] = useState(null);
 
     useEffect(() => {
-        axios.get('http://localhost:3001/api/currencies')
+        axios.get('https://currancy-converter.onrender.com/api/currencies')
             .then(response => setCurrencies(response.data))
             .catch(error => console.error(error));
     }, []);
 
     const handleConvert = () => {
         if (sourceCrypto && amount) {
-            axios.get('http://localhost:3001/api/convert', {
+            axios.get('http://currancy-converter.onrender.com/api/convert', {
                 params: {
                     sourceCrypto,
                     amount,
@@ -30,43 +31,42 @@ function App() {
 
     return (
         <div className="App">
-            <h1>Crypto Converter</h1>
-            <form>
-                <label>
-                    Source Cryptocurrency:
-                    <select value={sourceCrypto} onChange={(e) => setSourceCrypto(e.target.value)}>
-                        {currencies.map((currency) => (
-                            <option key={currency} value={currency}>
-                                {currency}
-                            </option>
-                        ))}
-                    </select>
-                </label>
-                <br />
-                <label>
-                    Amount:
-                    <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
-                </label>
-                <br />
-                <label>
-                    Target Currency:
-                    <select value={targetCurrency} onChange={(e) => setTargetCurrency(e.target.value)}>
-                        <option value="USD">USD</option>
-                        <option value="EUR">EUR</option>
-                        {/* Add more currencies if needed */}
-                    </select>
-                </label>
-                <br />
-                <button type="button" onClick={handleConvert}>
-                    Convert
-                </button>
-            </form>
-            <h1>helo:{convertedAmount}</h1>
-            {convertedAmount !== null && (
-                <p style={{color:"black"}}>
-                    Converted Amount: {convertedAmount} {targetCurrency}
-                </p>
-            )}
+            <div className='innerbox-app'>
+                <h1>Crypto Converter</h1>
+                <form>
+                    <label className='label'>
+                        Source Cryptocurrency:
+                        <select value={sourceCrypto} onChange={(e) => setSourceCrypto(e.target.value)}>
+                            {currencies.map((currency) => (
+                                <option key={currency} value={currency}>
+                                    {currency}
+                                </option>
+                            ))}
+                        </select>
+                    </label>
+                    <label className='label'>
+                        Amount:
+                        <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
+                    </label>
+                    <label className='label'>
+                        Target Currency:
+                        <select value={targetCurrency} onChange={(e) => setTargetCurrency(e.target.value)}>
+                            <option value="USD">USD</option>
+                            <option value="EUR">EUR</option>
+                            {/* Add more currencies if needed */}
+                        </select>
+                    </label>
+                    <br />
+                    <button type="button" onClick={handleConvert} className='bttn'>
+                        Convert
+                    </button>
+                </form>
+                {convertedAmount !== null && (
+                    <p>
+                        Converted Amount: {convertedAmount} {targetCurrency}
+                    </p>
+                )}
+            </div>
         </div>
     );
 }
